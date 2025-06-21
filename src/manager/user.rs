57f2ad00dcd4c78943dc721b60e8c60c04a74b53
Matthew Schwartz <matthew.schwartz@linux.dev>
2025-06-21 23:01:21 +0800

    wip: allow custom InputPlumber target devices in configuration file
    Some devices may need more than only one InputPlumber target device, i.e.
    the MSI Claw series needs a `keyboard` target in order to grab the volume
    keys. To account for this, lets add the ability to specify multiple
    targets within the device configuration files.

--- src/manager/user.rs
+++ src/manager/user.rs
@@ -1160,8 +1160,8 @@ mod test {
     use crate::daemon::user::UserContext;
     use crate::hardware::test::fake_model;
     use crate::hardware::{
-        BatteryChargeLimitConfig, DeviceConfig, DeviceMatch, DmiMatch, PerformanceProfileConfig,
-        RangeConfig, SteamDeckVariant, TdpLimitConfig,
+        BatteryChargeLimitConfig, DeviceConfig, DeviceMatch, DmiMatch, InputPlumberConfig,
+        PerformanceProfileConfig, RangeConfig, SteamDeckVariant, TdpLimitConfig,
     };
     use crate::platform::{
         FormatDeviceConfig, PlatformConfig, ResetConfig, ScriptConfig, ServiceConfig, StorageConfig,
@@ -1225,6 +1225,9 @@ mod test {
             performance_profile: Some(PerformanceProfileConfig {
                 platform_profile_name: String::from("power-driver"),
                 suggested_default: String::from("balanced"),
+            }),
+            inputplumber: Some(InputPlumberConfig {
+                target_devices: Some(vec![String::from("deck-uhid")]),
             }),
         })
     }
